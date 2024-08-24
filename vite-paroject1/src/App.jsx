f608d684597fd9cbefbfc1f1853f73eac45f17bd
Anurag123-cloud <anuragmaurya566@gmail.com>
2024-08-24 19:43:16 +0530

    file upload

---
+++ vite-paroject1/src/App.jsx
@@ -0,0 +1,27 @@
+
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+const[color ,setColor]=useState("yellow")
+
+
+return(
+<>
+  <div className=' w-full h-screen' style={{backgroundColor:color}}></div>
+  <div className='flex flex-wrap fixed bottom-12 inset-x-0 justify-center px-2  '>
+  <div className='bg-purple-600 p-2 rounded-lg'>
+  <button className=' m-2 p-2 bg-[red] rounded-lg text-white font-medium'  onClick={()=>setColor("red")}>change</button>
+  <button className=' m-2 p-2 bg-[blue] rounded-lg text-white font-medium' onClick={()=>setColor("blue")}>change</button>
+  <button className='m-2 p-2 bg-[green] rounded-lg text-white font-medium' onClick={()=>setColor("green")}>change</button>
+  <button className='p-2 m-2 bg-[orange] rounded-lg text-white font-medium' onClick={()=>setColor("orange")}>change</button>
+  </div>
+  </div>
+  
+  </>
+)
+}
+
+export default App
